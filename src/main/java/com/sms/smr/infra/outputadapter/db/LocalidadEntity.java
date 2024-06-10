e72a5d950b414fa9793aa1b71336d0a1c1c15cc2
dom061077@gmail.com <dom061077@gmail.com>
2024-06-10 00:37:31 -0300

    localidad

--- src/main/java/com/sms/smr/infra/outputadapter/db/LocalidadEntity.java
+++ src/main/java/com/sms/smr/infra/outputadapter/db/LocalidadEntity.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,7 +15,7 @@ import lombok.Setter;
 @Setter
 @Entity
 @NoArgsConstructor
-
+@Table(name="localidad")
 public class LocalidadEntity {
 
     @Id
