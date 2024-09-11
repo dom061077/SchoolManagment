4121fd5bae3432b98ecc5197a843cfa7fb1e7e87
dom061077@gmail.com <dom061077@gmail.com>
2024-09-11 13:33:32 -0300

    roles

--- src/main/java/com/sms/smr/infra/inputadapter/AuthenticationApi.java
+++ src/main/java/com/sms/smr/infra/inputadapter/AuthenticationApi.java
@@ -5,7 +5,10 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,20 +20,43 @@ import com.sms.smr.domain.AuthenticationResponse;
 import com.sms.smr.domain.RegisterRequest;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
+import static java.util.stream.Collectors.toSet;
 
-//@RestController
+@RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationApi {
 
-  private final AuthenticationUseCase service;
+  //private final AuthenticationUseCase service;
+
+
+
+  @GetMapping(value="/roles", produces = MediaType.APPLICATION_JSON_VALUE)
+  public List<String> gertRoles(){
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    Jwt jwt = (Jwt) authentication.getPrincipal();
+
+        var realmAccess = new HashMap<>(jwt.getClaim("realm_access"));
+
+        var roles =  realmAccess.get("roles");
+
+        //var roles = (Map<String, List<String>>)realmAccess.get("roles");
+
+        return (List<String>) roles;
+
+    
+  }
+
 
   //@PostMapping(value="/register",produces=MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<AuthenticationResponse> register(
+/*   public ResponseEntity<AuthenticationResponse> register(
       @RequestBody RegisterRequest request
   ) {
     return ResponseEntity.ok(service.register(request));
@@ -57,7 +83,7 @@ public class AuthenticationApi {
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.toList());
     }
-
+ */
 
 
 
