81b739742959d9bcc352f3289358fb7a8ae9abae
dom061077@gmail.com <dom061077@gmail.com>
2024-12-07 16:18:58 -0300

    gitrunner 10

--- src/test/java/com/sms/smr/SchoolManagment/SchoolManagmentApplicationTests.java
+++ src/test/java/com/sms/smr/SchoolManagment/SchoolManagmentApplicationTests.java
@@ -1,35 +1,23 @@
 package com.sms.smr.SchoolManagment;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertThat;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@DataJpaTest
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class SchoolManagmentApplicationTests {
 	
-    @Container
-    @ServiceConnection
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16.0");
+ //   @Container
+ //   @ServiceConnection
+  //  static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16.0");
 
 
 
     @Test
     void connectionEstablished() {
-        assertThat(postgres.isCreated()).isTrue();
-        assertThat(postgres.isRunning()).isTrue();
+       // assertThat(postgres.isCreated()).isTrue();
+        //assertThat(postgres.isRunning()).isTrue();
     }
 
 	@Test
