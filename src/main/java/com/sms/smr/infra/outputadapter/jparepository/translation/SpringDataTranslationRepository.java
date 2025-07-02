cba35e6623d09e868179683b50c13f529023416c
dom061077@gmail.com <dom061077@gmail.com>
2025-07-02 00:39:15 -0300

    Translationentity

--- src/main/java/com/sms/smr/infra/outputadapter/jparepository/translation/SpringDataTranslationRepository.java
+++ src/main/java/com/sms/smr/infra/outputadapter/jparepository/translation/SpringDataTranslationRepository.java
@@ -1,5 +1,10 @@
 package com.sms.smr.infra.outputadapter.jparepository.translation;
 
-public interface SpringDataTranslationRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import com.sms.smr.infra.outputadapter.db.TranslationEntity;
+
+public interface SpringDataTranslationRepository extends JpaRepository<TranslationEntity, Long> {
 
 }
--- src/main/java/com/sms/smr/infra/outputadapter/jparepository/translation/TranslationRepository.java
+++ src/main/java/com/sms/smr/infra/outputadapter/jparepository/translation/TranslationRepository.java
@@ -3,37 +3,45 @@ package com.sms.smr.infra.outputadapter.jparepository.translation;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.stereotype.Component;
+
+import com.sms.smr.domain.Translation;
 import com.sms.smr.infra.inputadapter.dto.query.QueryDto;
 import com.sms.smr.infra.outputadapter.db.TranslationEntity;
+import com.sms.smr.infra.outputadapter.jparepository.queryrepository.QueryRepository;
 import com.sms.smr.infra.outputport.EntityRepository;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+
+@AllArgsConstructor
+@Component(value = "translationRepository")
 public class TranslationRepository implements EntityRepository<TranslationEntity> {
 
-    static final SpringDataTranslationRepository sDataTranslationRepository = null;
+    private final SpringDataTranslationRepository sDataTranslationRepository;
+    private final QueryRepository queryRepository;
 
     @Override
     public TranslationEntity save(TranslationEntity reg) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'save'");
+        return sDataTranslationRepository.save(reg);
     }
 
     @Override
     public Optional<TranslationEntity> getById(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getById'");
+        return (Optional<TranslationEntity>) sDataTranslationRepository.findById(id);
     }
 
     @Override
     public List<TranslationEntity> getAll(int offset, int limit, List<QueryDto> queryFilters,
             List<QueryDto> sortFilters) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+        return (List<TranslationEntity>)queryRepository.getAllAnd(TranslationEntity.class, offset, limit, queryFilters, sortFilters);
     }
 
     @Override
     public long getCount(List<QueryDto> queryFilters) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getCount'");
+        return queryRepository.getCount(TranslationEntity.class, queryFilters);
     }
 
     @Override
