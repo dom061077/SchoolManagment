5acb08b914685eaf897c73f054ee19064d580a2b
danielmedina@synapticlinks.com.ar <danielmedina@synapticlinks.com.ar>
2025-10-13 09:53:38 -0300

    adding SchoolEntity

---
+++ src/main/java/com/sms/smr/infra/outputadapter/db/school/SchoolEntity.java
@@ -0,0 +1,28 @@
+package com.sms.smr.infra.outputadapter.db.school;
+
+import com.sms.smr.infra.outputadapter.db.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+
+@Entity
+@AllArgsConstructor
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@Table(name="escuela")
+public class SchoolEntity extends BaseEntity {
+
+    @NotNull
+    private String name;
+    @NotNull
+    private String cue;
+}
