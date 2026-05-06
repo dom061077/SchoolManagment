1e2022cc2d3e9de48866d0267610ebda60e7bb58
danielmedina@synapticlinks.com.ar <danielmedina@synapticlinks.com.ar>
2026-05-06 12:38:18 -0300

    feat: add AcademicYearApi controller to handle academic year REST endpoints

--- src/main/java/com/sms/smr/infra/input/rest/AcademicYearApi.java
+++ src/main/java/com/sms/smr/infra/input/rest/AcademicYearApi.java
@@ -1,5 +1,17 @@
 package com.sms.smr.infra.input.rest;
 
-public class AcademicYearApi {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sms.smr.domain.model.academic.AcademicYear;
+import com.sms.smr.domain.ports.in.BaseUseCase;
+
+@RestController
+@RequestMapping(value = "/api/v1/academic-year")
+public class AcademicYearApi extends BaseApi<AcademicYear, Long> {
+
+    public AcademicYearApi(BaseUseCase<AcademicYear, Long> academicYearUseCase) {
+        super(academicYearUseCase);
+    }
 
 }
